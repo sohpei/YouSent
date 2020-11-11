94e1d0475845f591c2a9189225863adf3a6365bc
DrainCena <josephkan3@gmail.com>
2020-11-10 20:58:33 -0500

    Added backend

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,13 +7,30 @@ class App extends React.Component{
         super(props)
         this.state = {
             test: "hello",
+            // accessToken: "",
         }
+
+    }
+
+    async testYoutube() {
+        return await Youtube.channel()
+    }
+
+    async authorizeGoogle() {
+        Youtube.authenticate(
+            '/search', {
+                params: {
+                    q: "cool"
+                }
+            }
+        )
     }
 
     render() {
         return(
             <div>
                 <h1>{this.state.test}</h1>
+                <button onClick={this.authorizeGoogle}></button>
             </div>
         )
     }
--- src/util/Youtube.js
+++ src/util/Youtube.js
@@ -1,8 +1,64 @@
+// // import gapi from "google-api-javascript-client"
+// import gapi from "./GoogleApi.js"
+// // import gapi from "gapi"
 
-const Youtube = {
-    
-}
+// var clientSecret = "88350370043-avr3t0otcj8dclqn8c70pm1vq1mp0ae1.apps.googleusercontent.com"
+// var apiKey = "AIzaSyBYigJ8EzMulO4hQDhSGXxZETonUqDoGrU"
 
 
 
-export default Youtube;
+
+
+// const Youtube = {
+  
+//     async authenticate() {
+//       await console.log(await gapi.auth2)
+//       await gapi.load("client:auth2", function() {
+//           gapi.auth2.init({client_id: clientSecret});
+//         });
+
+
+//         return await gapi.auth2.getAuthInstance()
+//         .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+//         .then(function() { console.log("Sign-in successful"); },
+//               function(err) { console.error("Error signing in", err); });
+//     },
+
+//     loadClient() {
+//         gapi.client.setApiKey("YOUR_API_KEY");
+//         return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+//             .then(function() { console.log("GAPI client loaded for API"); },
+//                   function(err) { console.error("Error loading GAPI client for API", err); });
+//     },
+
+//     getChannelByString(channelName) {
+//         return gapi.client.youtube.channels.list({
+//             "part": [
+//               "snippet"
+//             ],
+//             "forUsername": "Pewdiepie"
+//           })
+//               .then(function(response) {
+//                       // Handle the results here (response.result has the parsed body).
+//                       console.log("Response", response);
+//                     },
+//                     function(err) { console.error("Execute error", err); });
+//     }
+// }
+
+
+
+// export default Youtube;
+
+
+import axios from 'axios';
+const KEY = 'AIzaSyBYigJ8EzMulO4hQDhSGXxZETonUqDoGrU'; // mention your youtube API key here
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
